1c59a55db720399f5db17219541820165a8a452c
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-04-11 21:08:10 +0200

    dto - FeignApplication

---
+++ src/main/java/com/example/feign/dto/CarApiDto.java
@@ -0,0 +1,11 @@
+package com.example.feign.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CarApiDto {
+    private String brand;
+    private String model;
+}
